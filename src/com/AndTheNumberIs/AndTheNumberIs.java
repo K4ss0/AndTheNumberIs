96c69f55a0a74b991f379f34b4ac0ce5285089e8
K4ss0 <mikalandrs@gmail.com>
2023-07-16 17:30:48 -0700

    attempting to push new code

--- src/com/AndTheNumberIs/AndTheNumberIs.java
+++ src/com/AndTheNumberIs/AndTheNumberIs.java
@@ -10,40 +10,39 @@ public class AndTheNumberIs {
 		
 		Random random = new Random();
 		int guessMe = random.nextInt(100)+1;
-		for(int i = 0; i < 1 ; i = i + 1) {
+		 
 //			System.out.println("random number is " + guessMe);
-		}
-			System.out.println("Pick a number between 1 and 100.");
+		
+		System.out.println("Pick a number between 1 and 100.");
 		Scanner scanner = new Scanner(System.in);
-		Integer input = scanner.nextInt();
+		int maxGuess = 5;
+		int remainingGuesses = maxGuess;
 		
-		while (input < 0 | input > 100) {
+		
+		while (remainingGuesses > 0) {
+			int input = scanner.nextInt();
+			
+			if(input < 1 || input > 100) {
 			System.out.println("You're guess is not between 1 and 100, please try again");
-			input = scanner.nextInt();}
+			continue;
+		}
 		
-		int maxGuess = 4;
-		
-		for (int guessChance = 1; guessChance < maxGuess; guessChance++) {	
+			
 		if (input == guessMe) {
 			System.out.println("You win!");
-			break;
-		}else{
-		 while (input != guessMe) {
-			if(input > guessMe) {
+			scanner.close();
+			return;
+		}else if(input > guessMe) {
 				System.out.println("Please pick a lower number");
-		}else { if(input < guessMe)
+		}else if(input < guessMe) {
 				System.out.println ("Please pick a higher number");
-				}
-			input = scanner.nextInt();
-		 }
-		 if(guessChance == maxGuess)	
-			System.out.println("You lose!"); 
-			System.out.println("The number to guess was: " + guessMe);}
-		scanner.close();
-		
 		}
+		remainingGuesses--;
 	}
+	System.out.println("You lose!"); 
+	System.out.println("The number to guess was: " + guessMe);
+	scanner.close();
 }
-
+}
 
 
